720f06ae3238e01c377c49592d3ee3fd3aedde96
kayza_000 <rfhbnj12>
2019-08-04 00:50:54 +0300

    start

---
+++ Lib/House.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Lib;
+
+/**
+ * Created by PhpStorm.
+ * User: kayza_000
+ * Date: 03.08.2019
+ * Time: 23:21
+ */
+class House
+{
+    public static function getTempString()
+    {
+        echo "Hi";
+    }
+}
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kayza_000
+ * Date: 03.08.2019
+ * Time: 22:55
+ */
+
+use Lib\House;
+require_once __DIR__ . "/vendor/autoload.php";
+
+
+
+House::getTempString();
